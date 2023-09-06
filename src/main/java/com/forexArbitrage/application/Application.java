f97c5ecba169e8ee84bbfd3ea01b1517873cc75a
Anthony3301 <anthonypirvuti@gmail.com>
2023-09-06 14:10:28 -0400

    intial creation of spring project

--- src/main/java/com/forexArbitrage/application/Application.java
+++ src/main/java/com/forexArbitrage/application/Application.java
@@ -9,7 +9,7 @@ public class Application {
 	public static void main(String[] args) {
 
 		SpringApplication.run(Application.class, args);
-		System.out.println("here");
+		System.out.println("intial setup");
 	}
 
 }

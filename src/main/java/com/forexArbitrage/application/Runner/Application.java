43bb78636113ca6e809639bd0ff36ef45c8c9239
Anthony3301 <anthonypirvuti@gmail.com>
2023-09-06 23:11:54 -0400

    added currency validation functionality, json parsing, and fetching of names

--- src/main/java/com/forexArbitrage/application/Runner/Application.java
+++ src/main/java/com/forexArbitrage/application/Runner/Application.java
@@ -1,11 +1,14 @@
 package com.forexArbitrage.application.Runner;
 
 import com.forexArbitrage.application.verification.CurrencyVerifier;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.forexArbitrage.application")
 public class Application {
 	@Autowired
 	CurrencyVerifier currVer;
@@ -16,4 +19,13 @@ public class Application {
 		System.out.println("intial setup");
 	}
 
+	@PostConstruct
+	public void runAfterStartup() {
+		System.out.println("Loading all currencies...");
+		if (currVer.getCurrencyCache()) {
+			System.out.println("Moving on!");
+		} else {
+			System.out.println("Failed to load currencies.");
+		}
+	}
 }
--- src/main/java/com/forexArbitrage/application/verification/CurrencyVerifier.java
+++ src/main/java/com/forexArbitrage/application/verification/CurrencyVerifier.java
@@ -1,23 +1,38 @@
 package com.forexArbitrage.application.verification;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import org.apache.catalina.connector.InputBuffer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.*;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
 @Component
 public class CurrencyVerifier {
+
+    // utilities for reading the api for currencies
     private String apiVersion = "cbb41bd8ff49f43f98a68d9a4e0ff88947a07e97";
     private String date = "latest";
     private String endPoint = "currencies";
-
     private static HttpURLConnection connection;
-
     private StringBuffer response = new StringBuffer();
 
-    public void loadCurrencies() {
+    // parsed json to hashMap
+    private HashMap<String, String> currencies;
+
+    // API call to cache of all valid currencies worldwide
+    private void loadCurrencies() {
         String httpRequest = String.format("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@%s/%s/%s.json",
                 apiVersion, date, endPoint);
 
@@ -45,10 +60,46 @@ public class CurrencyVerifier {
         } catch (Exception e) {
             System.out.println("Malformed API URL or improper usage");
             e.printStackTrace();
-        } finally {
-            System.out.println(response.toString());
+        }
+    }
+
+    // parses the json string into a hashmap of (currency code, currency name)
+    private void parseJson() {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            TypeReference<HashMap<String, String>> typeReference = new TypeReference<HashMap<String, String>>(){};
+            currencies = objectMapper.readValue(response.toString(), typeReference);
+
+            System.out.printf("Loaded %d currencies!%n", currencies.size());
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // wrapper function to simplify the cache process
+    public boolean getCurrencyCache() {
+        try {
+            loadCurrencies();
+            parseJson();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    // checks if the code or name of the currency is valid
+    public boolean isValidCurrency(String currency) {
+        currency = currency.toLowerCase(Locale.ROOT).strip();
+
+        for (Map.Entry<String,String> pair : currencies.entrySet()) {
+            if (pair.getKey().equals(currency) || pair.getValue().equals(currency)) {
+                return true;
+            }
         }
 
-
+        return false;
     }
 }
